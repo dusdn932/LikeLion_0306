8aab7cbf4af28af40a46fa9ac8922c2f1c10b544
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-03-06 13:05:14 +0900

    0306_오전강의2
    간단한 텍스트게임1

---
+++ ConsoleApp2/ConsoleApp2/warrior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    public class warrior : GameCharacter
+    {
+        public warrior(string name) : base(name, 100, 15, 10)
+        {
+
+        }
+        public override void BasicAttack(GameCharacter target)
+        {
+            Console.WriteLine($"{Name}가 {target.Name}에게 기본 공격을 시도합니다!");
+            target.TakeDamage(Attack);
+        }
+
+        public override void SpecialAttack(GameCharacter target)
+        {
+            Console.WriteLine($"{Name}가 {target.Name}에게 휠윈드을 시전합니다!");
+            target.TakeDamage(Attack * 2);
+        }
+    }
+}
